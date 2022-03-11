b3450d2837e6fcf1d7f642ffba0338e258b29139
natgefe <99477215+natgefe@users.noreply.github.com>
2022-03-11 06:22:21 +0300

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main (void)
+/**
+ * main - Entry point
+ *
+ * Description: prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 (Success)
+*/
+
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
