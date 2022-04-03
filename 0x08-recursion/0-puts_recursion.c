913234611bf0a12fa5692fafdadae3b10e46fa68
natgefe <99477215+natgefe@users.noreply.github.com>
2022-04-03 04:12:10 +0300

    1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,14 +6,18 @@
  *
  * Return: void
  */
+
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
+	{
+		_putchar(*(s + 0));
+		_puts_recursion((s + 1));
+	}
+	else 
 	{
 		_putchar('\n');
 		return;
 	}
-	_putchar(*s);
-	s++;
-	_puts_recursion(s + 1);
+
 }
